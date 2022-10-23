f1eca5baad7754f346f9cdb59c490fb0245a53ba
alejandbel <bogdanov28c@gmail.com>
2022-10-23 12:51:28 +0300

    Extracted methods

--- homework_1/reverse_or_rotate/src/solution.ts
+++ homework_1/reverse_or_rotate/src/solution.ts
@@ -1,27 +1,43 @@
 export function revRot(s: string, sz: number): string {
-  if (sz <= 0 || s.length === 0 || sz > s.length) {
+  if (isInputInvalid(s, sz)) {
     return '';
   }
-  const chunks: string[] | null = s.match(new RegExp(`.{${sz}}`,'g'));
+  const chunks: string[] | null = s.match(new RegExp(`.{${sz}}`, 'g'));
 
   if (!chunks) {
     return '';
   }
 
   for (let i = 0; i < chunks.length; ++i) {
-    const odds: number = chunks[i].split('').reduce((acc, cur) => {
-      if (parseInt(cur) % 2 === 1) {
-        return acc + 1;
-      }
-      return acc;
-    }, 0);
+    const odds: number = getOdds(chunks, i);
 
     if (odds % 2 === 0) {
-      chunks[i] = chunks[i].split('').reverse().join('');
+      reverseChunk(chunks, i);
     } else {
-      chunks[i] = chunks[i].slice(1, sz) + chunks[i][0];
+      rotateChunkLeft(chunks, i, sz);
     }
   }
 
   return chunks.join('');
+}
+
+function isInputInvalid(s: string, sz: number): boolean {
+  return sz <= 0 || s.length === 0 || sz > s.length;
+}
+
+function reverseChunk(chunks: string[], i: number): void {
+  chunks[i] = chunks[i].split('').reverse().join('');
+}
+
+function rotateChunkLeft(chunks: string[], i: number, sz: number): void {
+  chunks[i] = chunks[i].slice(1, sz) + chunks[i][0];
+}
+
+function getOdds(chunks: string[], i: number): number {
+  return chunks[i].split('').reduce((acc, cur) => {
+    if (parseInt(cur) % 2 === 1) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
